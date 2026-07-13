4f72c6093b961bb7284be2e02dcabe9e4d626c8a
Elden <elden.samuel.n@gmail.com>
2026-07-14 00:57:19 +0800

    renamed executable

--- build.zig
+++ build.zig
@@ -45,7 +45,7 @@ pub fn build(b: *std.Build) void {
     // in this directory.
     // If neither case applies to you, feel free to delete the declaration you
     const exe = b.addExecutable(.{
-        .name = "dsb",
+        .name = "zsb",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
